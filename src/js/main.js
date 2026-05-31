905c9ae08aade9dc5963096adc75077a9bf5f9c3
laiscrz <lais.albino2003@gmail.com>
2026-05-31 18:54:38 -0300

    feat: updated files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import { initNavbar } from './navbar.js';
 import { loadSections } from './loadSections.js';
 import { initAlbumModal } from './albumModal.js';
+import './cultureCarousel.js';
 
 async function init(){
 
